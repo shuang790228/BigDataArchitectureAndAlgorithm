f26ab9d78774ca578ac7ddac81e2ce8e685f418f
Shen Huang <shenhuan@us.ibm.com>
2017-01-30 15:42:14 -0800

    no message

---
+++ Search/Elasticsearch/elasticsearch-analysis-ik/src/main/java/org/elasticsearch/elasticsearch_analysis_ik/App.java
@@ -0,0 +1,13 @@
+package org.elasticsearch.elasticsearch_analysis_ik;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
